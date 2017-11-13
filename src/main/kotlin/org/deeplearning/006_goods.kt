fe61c97ab987a0478b0957a18591907d7eb540fb
Zhenglai Zhang <zhenlaizhang@hotmail.com>
2017-11-13 21:57:43 +0800

    sealed class

--- src/main/kotlin/org/deeplearning/006_goods.kt
+++ src/main/kotlin/org/deeplearning/006_goods.kt
@@ -133,6 +133,24 @@ data classes have to fulfil the following requirements:
 
 // Component functions generated for data classes enable their use in destructuring declarations:
 
+
+// A sealed class is abstract by itself, it cannot be instantiated directly and can have abstract members.
+// Sealed classes are not allowed to have non-private constructors (their constructors are private by default).
+// classes which extend subclasses of a sealed class (indirect inheritors) can be placed anywhere, not necessarily in the same file
+// The key benefit of using sealed classes comes into play when you use them in a when expression. If it's possible to verify that the statement covers all cases, you don't need to add an else clause to the statement. However, this works only if you use when as an expression (using the result) and not as a statement.
+sealed class Expr
+
+data class Const(val number: Double) : Expr()
+data class Sum(val e1: Expr, val e2: Expr) : Expr()
+object NotANumber : Expr()
+
+fun eval(expr: Expr): Double = when (expr) {
+    is Const -> expr.number
+    is Sum -> eval(expr.e1) + eval(expr.e2)
+    NotANumber -> Double.NaN
+    // the `else` clause is not required because we've covered all the cases
+}
+
 fun main(args: Array<String>) {
     l.swap(0, 2)
     println(l)
