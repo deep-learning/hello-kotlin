6b33a9b0dae1827667d413bdcc4e8f484453b111
Zhenglai Zhang <zhenlaizhang@hotmail.com>
2017-11-13 21:08:01 +0800

    extension functions

--- src/main/kotlin/org/deeplearning/006_goods.kt
+++ src/main/kotlin/org/deeplearning/006_goods.kt
@@ -4,8 +4,9 @@ package org.deeplearning
 Kotlin supports extension functions and extension properties.
 
 Extensions are resolved statically
-
 We would like to emphasize that extension functions are dispatched statically, i.e. they are not virtual by receiver type. This means that the extension function being called is determined by the type of the expression on which the function is invoked, not by the type of the result of evaluating that expression at runtime.
+Most of the time we define extensions on the top level, i.e. directly under packages
+To use such an extension outside its declaring package, we need to import it at the call site
  */
 
 // To declare an extension function, we need to prefix its name with a receiver type, i.e. the type being extended
@@ -36,6 +37,56 @@ fun C1.foo() = "c"
 
 fun D.foo() = "d"
 
+
+class D1 {
+    fun bar() {}
+
+    override fun toString(): String {
+        return super.toString()
+    }
+}
+
+class E1 {
+    fun baz() {}
+
+    fun D1.foo() {
+        bar() // calls D.bar(), extension receiver
+        baz() // calls E1.baz(), dispatch receiver
+    }
+
+    fun caller(d: D1) {
+        d.foo() // calls the extension function
+    }
+
+    // In case of a name conflict between the members of the dispatch receiver and the extension receiver,
+    // the extension receiver takes precedence.
+    // To refer to the member of the dispatch receiver you can use the qualified this syntax.
+    fun D1.bar() {
+        toString()              // D.toString()
+        this@E1.toString()      // C.toString()
+    }
+}
+
+// Extensions declared as members can be declared as open and overridden in subclasses. This means that the dispatch of such functions is virtual with regard to the dispatch receiver type, but static with regard to the extension receiver type.
+open class DD {}
+class D11: DD() {}
+
+open class CC {
+    open fun DD.foo() {
+        println("D.foo in C")
+    }
+
+    open fun D11.foo() {
+        println("D1.foo in C")
+    }
+
+    fun caller(d: DD) {
+        d.foo() // calls the extension method
+    }
+}
+
+// todo https://kotlinlang.org/docs/reference/extensions.html
+
 fun printFoo(c: C1) {
     println(c.foo())
     println(c.foo(1))
@@ -46,8 +97,25 @@ If a class has a member function, and an extension function is defined which has
 it's perfectly OK for extension functions to overload member functions which have the same name but a different signature:
  */
 
+// extension properties
+// Note that, since extensions do not actually insert members into classes, there's no efficient way for an extension property to have a backing field. This is why initializers are not allowed for extension properties. Their behavior can only be defined by explicitly providing getters/setters.
+val <T> List<T>.lastIndex2: Int
+    get() = size - 1
+
+class MyClass {
+    companion object {}
+}
+
+// companion object extension
+//define extension functions and properties for the companion object
+fun MyClass.Companion.foo() {
+
+}
+
 fun main(args: Array<String>) {
     l.swap(0, 2)
     println(l)
     println(D())    // "c
+    println(null.toString())
+    MyClass.foo()
 }
