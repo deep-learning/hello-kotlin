342bbbc0a3e3e62924fd2707651220c41c028d89
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-11-13 16:41:49 +0800

    hello interface

--- src/main/kotlin/org/deeplearning/005_class.kt
+++ src/main/kotlin/org/deeplearning/005_class.kt
@@ -5,9 +5,73 @@ class Empty
 class Invoice {}
 
 /*
+Functions, properties and classes, objects and interfaces can be declared on the "top-level", i.e. directly inside a package:
+    If you do not specify any visibility modifier, public is used by default, which means that your declarations will be visible everywhere;
+    If you mark a declaration private, it will only be visible inside the file containing the declaration;
+    If you mark it internal, it is visible everywhere in the same module;
+    protected is not available for top-level declarations.
+ */
+fun baz() {}
+
+private fun pbaz() {}
+class Bara {}
+
+public var bar: Int = 4
+internal val baz = 5
+
+/*
+For members declared inside a class:
+
+private means visible inside this class only (including all its members);
+protected — same as private + visible in subclasses too;
+internal — any client inside this module who sees the declaring class sees its internal members;
+public — any client who sees the declaring class sees its public members.
+NOTE for Java users: outer class does not see private members of its inner classes in Kotlin.
+
+If you override a protected member and do not specify the visibility explicitly, the overriding member will also have protected visibility.
+
+
+Modules:
+The internal visibility modifier means that the member is visible within the same module. More specifically, a module is a set of Kotlin files compiled together:
+    an IntelliJ IDEA module;
+    a Maven project;
+    a Gradle source set;
+    a set of files compiled with one invocation of the Ant task.
+ */
+open class Outer {
+    private val a = 1
+    protected open val b = 2
+    internal val c = 3
+    val d = 4  // public by default
+
+    protected class Nested {
+        public val e: Int = 5
+    }
+}
+
+class Subclass : Outer() {
+    // a is not visible
+    // b, c and d are visible
+    // Nested and e are visible
+
+    override val b = 5   // 'b' is protected
+}
+
+/*
+Here the constructor is private. By default, all constructors are public, which effectively amounts to them being visible everywhere where the class is visible (i.e. a constructor of an internal class is only visible within the same module).
+ */
+class Unrelated private constructor(o: Outer) {
+    // o.a, o.b are not visible
+    // o.c and o.d are visible (same module)
+    // Outer.Nested is not visible, and Nested::e is not visible either
+}
+
+/*
 Interfaces in Kotlin are very similar to Java 8. They can contain declarations of abstract methods, as well as method implementations. What makes them different from abstract classes is that interfaces cannot store state. They can have properties but these need to be abstract or to provide accessor implementations.
 
  A property declared in an interface can either be abstract, or it can provide implementations for accessors. Properties declared in interfaces can't have backing fields, and therefore accessors declared in interfaces can't reference them
+
+Classes, objects, interfaces, constructors, functions, properties and their setters can have visibility modifiers. (Getters always have the same visibility as the property.) There are four visibility modifiers in Kotlin: private, protected, internal and public. The default visibility, used if there is no explicit modifier, is public
  */
 interface MyInterface {
     fun bar()
